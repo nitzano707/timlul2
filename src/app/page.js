c733db62b0f538d1e15ef19fbe7700fdbaf77495
nitzano707 <nitzan.orot@gmail.com>
2024-10-26 21:40:55 +0300

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import AudioTranscription from './components/AudioTranscription';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <AudioTranscription />
+    </main>
+  );
+}
